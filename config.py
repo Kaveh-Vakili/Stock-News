547bb8993acae099a33e8394e8d735a8c650ef77
Kaveh Vakili <vakilik7@gmail.com>
2024-01-11 17:55:32 -0500

    created config file to read api keys

---
+++ config.py
@@ -0,0 +1,15 @@
+# all these need to be changed to valid credentials
+
+# Alpha Vantage API key, from https://www.alphavantage.co/
+AV_API_KEY = "O6SDHQUY6YPFWJAA"
+
+# News API key, from https://newsapi.org/
+NEWS_API_KEY = "44ec0cf826034511b55405586b50b2db"
+
+# Twilio SID & TOKEN, from https://www.twilio.com/
+TWILIO_SID = "mytwiliosid"
+TWILIO_TOKEN = "mytwiliotoken"
+# Twilio phone number to send the SMS from
+TWILIO_NUMBER = "notmynumber"
+# real phone number to send the SMS to
+TARGET_NUMBER = "mynumber"
--- main.py
+++ main.py
@@ -1,37 +1,110 @@
 import requests
 import datetime as dt
+from twilio.rest import Client
+import config
+from twilio.base.exceptions import TwilioRestException
 
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
-STOCK_ENDPOINT = "https://www.alphavantage.co/query"
-NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+AV_API_URL = "https://www.alphavantage.co/query"
+NEWS_API_URL = "https://newsapi.org/v2/everything"
 
-API_KEY="O6SDHQUY6YPFWJAA"
+#API_KEY="O6SDHQUY6YPFWJAA"
 
-NEWS_KEY="44ec0cf826034511b55405586b50b2db"
+#NEWS_KEY="44ec0cf826034511b55405586b50b2db"
 
+MIN_CHANGE = 5
 
-
-def price_check(name):
-    params={
-      "function": "TIME_SERIES_DAILY",
-    "symbol":STOCK_NAME,
-    "apikey":API_KEY,
-}
-        
-response=requests.get(STOCK_ENDPOINT,params=params)     
+def check_price(name):
 
 
-    
-def change_calculation(data):
-    
-    dataes=list(data)
-    
-    
-    
-    
+    params = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": name,
+        "outputsize": "compact",
+        "datatype": "json",
+        "apikey": config.AV_API_KEY
+    }
+    response = requests.get(AV_API_URL, params=params)
+
+    if response.status_code != 200:
+        print(response.text)
+        print("Make sure the AV_API_KEY is set properly in config.py.")
+
+    return response.json()["Time Series (Daily)"]
+
+
+def calculate_change(data):
+
+
+    dates = list(data)
+    yesterday_closing = float(data[dates[0]]["4. close"])
+    day_before_closing = float(data[dates[1]]["4. close"])
+
+    return (yesterday_closing - day_before_closing) / day_before_closing * 100
+
+
+def get_news(name):
+    """Takes a company name as a STR and returns a LIST of top related articles."""
+
+    today = dt.datetime.today().strftime("%Y-%m-%d")
+    params = {
+        "q": f"{name}&",
+        "from": f"{today}&",
+        "sortBy": "popularity&",
+        "apikey": config.NEWS_API_KEY
+    }
+    response = requests.get(NEWS_API_URL, params=params)
+    if response.status_code != 200:
+        print(response.text)
+        print("Make sure the NEWS_API_KEY is set properly in config.py.")
+        response.raise_for_status()
+    # return articles only
+    return response.json()["articles"]
+
+
+def send_sms(sms_text):
+    """Takes a message as STR and sends it as an SMS to the defined number."""
+    # re-using the code from Day 35, with minor changes
+    try:
+        client = Client(config.TWILIO_SID, config.TWILIO_TOKEN)
+        message = client.messages.create(body=sms_text, from_=config.TWILIO_NUMBER, to=config.TARGET_NUMBER)
+    except TwilioRestException as ex:
+        # a generic error message that will get displayed with each failed attempt
+        print(ex)
+        print("Make sure the TWILIO_SID, TWILIO_TOKEN, TWILIO_NUMBER and TARGET_NUMBER are set properly in config.py.")
+    else:
+        print(message.status)
+
+
+daily_data = check_price(STOCK_NAME)
+
+
+change = calculate_change(daily_data)
+
+n
+if abs(change) >= MIN_CHANGE:
+    message_head = f"{STOCK_NAME}: "
+    if change >= MIN_CHANGE:
+        message_head += "🔺"
+    else:
+        message_head += "🔻"
+    # include the percentage
+    message_head += f"{abs(round(change, 1))}%\n"
+
+    # get news data for the company name
+    news_data = get_news(COMPANY_NAME)
+    # send a message with the top three articles
+    for i in range(3):
+        # clear the content before each iteration
+        message_content = ""
+        # add the titles and descriptions
+        message_content += f"Headline: {news_data[i]['title']}\n"
+        message_content += f"Brief: {news_data[i]['description'].strip('Summary List Placement')}"
+        # send the sms
+        send_sms(message_head + message_content)
 
 
 
@@ -41,56 +114,3 @@ def change_calculation(data):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-response=requests.get(STOCK_ENDPOINT,params=stock_parameters)
-
-
-
-data=response.json()["Time Series (Daily)"]
-
-data_list=  [value for(key,value)  in data.items()]
-
-
-yesterday_Data=data_list[0]
-
-yesterday_Closing_Price=yesterday_Data["4. close"]
-
-two_days_Ago_Data=data_list[1]
-
-two_days_Ago_Price=two_days_Ago_Data["4. close"]
-
-difference=abs(float(yesterday_Closing_Price)-float(two_days_Ago_Price))
-
-
-difference_percent=(difference/float(yesterday_Closing_Price)*100)
-print(difference_percent)
-
-if(difference_percent>0.1):
-   news_parameters={
-       
-     "apiKEY":NEWS_KEY,
-     "qInTitle":COMPANY_NAME
-       
-   }
-the_news_response=requests.get(NEWS_ENDPOINT,params=news_parameters)
-theArticles=the_news_response.json()["articles"]
-print(theArticles)
-
-firstThreeArticles=theArticles[:3]
-
-print(firstThreeArticles)
-
-
-
-     
